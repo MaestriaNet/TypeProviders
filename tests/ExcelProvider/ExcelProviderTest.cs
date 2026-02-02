a64d1e1551873902cf1c71eae2cf9500a5d70649
Fábio Naspolini <fabionaspolini@gmail.com>
2026-02-02 00:54:47 -0300

    test: melhorar testes unitários
    - Modificado o caminho dos arquivos Excel utilizados nos testes para um formato mais relativo.
    - Atualizadas as configurações do projeto para o .NET 10.0, incluindo a habilitação de usings implícitos e suporte a nullable.
    - Adicionadas novas dependências necessárias para o projeto.
    - Ajustadas as asserções nos testes para refletir as mudanças nos arquivos e garantir a integridade dos dados.

--- tests/ExcelProvider/ExcelProviderTest.cs
+++ tests/ExcelProvider/ExcelProviderTest.cs
@@ -1,8 +1,7 @@
 using NUnit.Framework;
-using System.IO;
 
 // Top level statement support
-[ExcelProvider(TemplatePath = @"../../resources/Excel.xlsx")]
+[ExcelProvider(TemplatePath = @"./TestFile.xlsx")]
 public partial class MyExcelDataGlobalNamespace
 {
 }
@@ -10,57 +9,60 @@ public partial class MyExcelDataGlobalNamespace
 namespace Maestria.TypeProviders.Test.ExcelProvider
 {
     // Default is used sheet 1
-    [ExcelProvider(TemplatePath = @"../../resources/Excel.xlsx")]
+    [ExcelProvider(TemplatePath = @"./TestFile.xlsx")]
     public partial class MyExcelData
     {
     }
-
-    // Its possible set "SheetName" or "SheePosition" to load data struct of another page
-    [ExcelProvider(TemplatePath = @"../../resources/Excel.xlsx", SheetName = "Plan2")]
+    
+    // // Its possible set "SheetName" or "SheePosition" to load data struct of another page
+    [ExcelProvider(TemplatePath = @"./TestFile.xlsx", SheetName = "Plan2")]
     public partial class MyExcelDataSheet2
     {
     }
 
     public class ExcelProviderTest
     {
-        [Test]
-        public void Validated_Generated_ExcelExtensions()
+        [TestCase("../../../../generated/tests/Maestria.TypeProviders/Maestria.TypeProviders.Excel.ExcelGenerator/ExcelExtensions.g.cs")]
+        [TestCase("../../../../generated/tests/Maestria.TypeProviders/Maestria.TypeProviders.Excel.ExcelGenerator/ExcelGeneratorException.g.cs")]
+        [TestCase("../../../../generated/tests/Maestria.TypeProviders/Maestria.TypeProviders.Excel.ExcelGenerator/ExcelProviderAttribute.g.cs")]
+        public void GeneratedFile_AreExists_AndNotEmpty(string path)
         {
-            var generated = File.ReadAllLines("../../../../generated/tests/Maestria.TypeProviders/Maestria.TypeProviders.Excel.ExcelGenerator/ExcelExtensions.cs");
-            var expected = File.ReadAllLines("../../../ExcelProvider/ExpectedFiles/ExcelExtensions.expected");
-            Assert.AreEqual(expected, generated);
+            Assert.That(File.Exists(path), "Expected source generator not create expected file: " + path);
+            
+            var fileContent = File.ReadAllText(path);
+            Assert.That(fileContent, Is.Not.Empty, "Expected source generator create a empty file: " + path);
         }
 
         [Test]
-        public void Validated_Generated_ExcelProviderAttribute()
+        public void LoadExcelFile_MustBe_Success()
         {
-            var generated = File.ReadAllLines("../../../../generated/tests/Maestria.TypeProviders/Maestria.TypeProviders.Excel.ExcelGenerator/ExcelProviderAttribute.cs");
-            var expected = File.ReadAllLines("../../../ExcelProvider/ExpectedFiles/ExcelProviderAttribute.expected");
-            Assert.AreEqual(expected, generated);
-        }
+            var filePath = @"../../../ExcelProvider/TestFile.xlsx";
+            var data = MyExcelDataFactory.Load(filePath).ToArray();
+            Assert.That(data, Is.Not.Null);
+            Assert.That(data.Length, Is.EqualTo(6));
 
-        [Test]
-        public void Validated_Generated_MyExcelData()
-        {
-            var generated = File.ReadAllLines("../../../../generated/tests/Maestria.TypeProviders/Maestria.TypeProviders.Excel.ExcelGenerator/Maestria.TypeProviders.Test.ExcelProvider.MyExcelData.ExcelTypeProvider.cs");
-            var expected = File.ReadAllLines("../../../ExcelProvider/ExpectedFiles/Maestria.TypeProviders.Test.ExcelProvider.MyExcelData.ExcelTypeProvider.expected");
-            Assert.AreEqual(expected, generated);
-        }
-
-        [Test]
-        public void Validated_Generated_MyExcelDataSheet2()
-        {
-            var generated = File.ReadAllLines("../../../../generated/tests/Maestria.TypeProviders/Maestria.TypeProviders.Excel.ExcelGenerator/Maestria.TypeProviders.Test.ExcelProvider.MyExcelDataSheet2.ExcelTypeProvider.cs");
-            var expected = File.ReadAllLines("../../../ExcelProvider/ExpectedFiles/Maestria.TypeProviders.Test.ExcelProvider.MyExcelDataSheet2.ExcelTypeProvider.expected");
-            Assert.AreEqual(expected, generated);
-        }
-
-        [Test]
-        public void Validated_Generated_MyExcelDataGlobalNamespace()
-        {
-            var generated = File.ReadAllLines("../../../../generated/tests/Maestria.TypeProviders/Maestria.TypeProviders.Excel.ExcelGenerator/MyExcelDataGlobalNamespace.ExcelTypeProvider.cs");
-            var expected = File.ReadAllLines("../../../ExcelProvider/ExpectedFiles/MyExcelDataGlobalNamespace.ExcelTypeProvider.expected");
-            Assert.AreEqual(expected, generated);
+            for (var arrayIndex = 0; arrayIndex < data.Length; arrayIndex++)
+            {
+                var rowNumber = arrayIndex + 1;
+                Assert.That(data[arrayIndex].Id, Is.EqualTo(rowNumber));
+                Assert.That(data[arrayIndex].Name, Is.EqualTo($"Name {rowNumber}"));
+                Assert.That(data[arrayIndex].BirthDate, Is.EqualTo(new DateTime(1987, 03, rowNumber)));
+                Assert.That(data[arrayIndex].Prop, Is.EqualTo(rowNumber));
+                Assert.That(data[arrayIndex].CalculatedProp, Is.EqualTo(rowNumber + 10));
+                Assert.That(data[arrayIndex].PropSample, Is.EqualTo($"Teste {rowNumber + 10}"));
+                Assert.That(data[arrayIndex].PropSample2, Is.EqualTo($"Teste {rowNumber + 20}"));
+                Assert.That(data[arrayIndex].Prop_Sample3, Is.EqualTo($"Teste {rowNumber + 30}"));
+                Assert.That(data[arrayIndex].Prop_Sample4, Is.EqualTo($"Teste {rowNumber + 40}"));
+                Assert.That(data[arrayIndex].Prop_Sample5, Is.EqualTo($"Teste {rowNumber + 50}"));
+                Assert.That(data[arrayIndex].Prop_Sample_AB, Is.EqualTo($"Teste {rowNumber + 60}"));
+            }
+            
+            Assert.That(data[0].Value, Is.Null);
+            Assert.That(data[1].Value, Is.EqualTo(5));
+            Assert.That(data[2].Value, Is.EqualTo(5.01d));
+            Assert.That(data[3].Value, Is.EqualTo(7d));
+            Assert.That(data[4].Value, Is.Null);
+            Assert.That(data[5].Value, Is.EqualTo(9d));
         }
     }
 }
